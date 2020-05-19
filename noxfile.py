31d0b717c09728ac5acbdc55c259adfa7821c623
Stein Magnus Jodal <stein.magnus@jodal.no>
2020-05-20 00:21:37 +0200

    Setup mypy type checker

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,9 @@
 import nox
 
 
+locations = ["src", "tests", "noxfile.py"]
+
+
 @nox.session(python=["3.7", "3.8"])
 def tests(session):
     args = session.posargs or ["--cov"]
@@ -10,8 +13,15 @@ def tests(session):
 
 @nox.session(python="3.8")
 def flake8(session):
-    args = session.posargs or []
+    args = session.posargs or locations
     session.install(
         "flake8", "flake8-black", "flake8-bugbear", "flake8-import-order"
     )
     session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def mypy(session):
+    args = session.posargs or locations
+    session.install("mypy")
+    session.run("mypy", *args)
