f0c94a78306fbe1331ff64a731cf24fad3abbb26
Stein Magnus Jodal <stein.magnus@jodal.no>
2020-08-10 16:06:53 +0200

    GS1 check digit calculation (#6)
    * Add GS1 check digit calculation for numeric values
    
    * Add GS1 check digit calculation for price/weight fields

---
+++ tests/test_gs1_check_digit.py
@@ -0,0 +1,70 @@
+import pytest
+
+from biip.gs1_check_digit import (
+    numeric_check_digit,
+    price_check_digit,
+)
+
+
+def test_numeric_check_digit_with_nonnumeric_value() -> None:
+    with pytest.raises(ValueError) as exc_info:
+        numeric_check_digit("abc")
+
+    assert str(exc_info.value) == "Expected numeric value, got 'abc'."
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        # Example from reference
+        ("37610425002123456", 9),
+        # GTIN-13 from a product
+        ("629104150021", 3),
+        ("703801005447", 1),
+        # ISBN from a book
+        ("978820551237", 5),
+        # GLN for an organization
+        ("708000382434", 9),
+    ],
+)
+def test_numeric_check_digit(value: str, expected: int) -> None:
+    assert numeric_check_digit(value) == expected
+
+
+def test_price_check_digit_with_nonnumeric_value() -> None:
+    with pytest.raises(ValueError) as exc_info:
+        price_check_digit("abc")
+
+    assert str(exc_info.value) == "Expected numeric value, got 'abc'."
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        # Too short
+        "123",
+        # Too long
+        "123456",
+    ],
+)
+def test_price_check_digit_on_values_with_wrong_length(value: str) -> None:
+    with pytest.raises(ValueError) as exc_info:
+        price_check_digit(value)
+
+    assert (
+        str(exc_info.value)
+        == f"Expected input of length 4 or 5, got {value!r}."
+    )
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        # Four-digit price field
+        ("2875", 39),
+        # Five-digit price field
+        ("14685", 6),
+    ],
+)
+def test_price_check_digit(value: str, expected: int) -> None:
+    assert price_check_digit(value) == expected
