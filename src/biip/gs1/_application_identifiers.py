1ddb1517f98aa07d5fddd6fbf1b17e684b19f75e
Stein Magnus Jodal <stein.magnus@jodal.no>
2020-08-14 12:59:24 +0200

    Remove unused function

--- src/biip/gs1/_application_identifiers.py
+++ src/biip/gs1/_application_identifiers.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import json
 import pathlib
 from dataclasses import dataclass, field
-from typing import Optional, Type
+from typing import Type
 
 from biip import ParseError
 
@@ -90,42 +90,6 @@ class GS1ApplicationIdentifier:
     def __len__(self: GS1ApplicationIdentifier) -> int:
         """Get the length of the Application Identifier code."""
         return len(self.ai)
-
-
-def get_predefined_length(value: str) -> Optional[int]:
-    """Get predefined length of element string, including Application Identifier prefix.
-
-    Args:
-        value: Any string starting with a GS1 Application Identifier.
-
-    Returns:
-        Predefined length or ``None`` if undefined.
-    """
-    ai_prefix = value[:2]
-    return {
-        "00": 20,
-        "01": 16,
-        "02": 16,
-        "03": 16,
-        "04": 18,
-        "11": 8,
-        "12": 8,
-        "13": 8,
-        "14": 8,
-        "15": 8,
-        "16": 8,
-        "17": 8,
-        "18": 8,
-        "19": 8,
-        "20": 4,
-        "31": 10,
-        "32": 10,
-        "33": 10,
-        "34": 10,
-        "35": 10,
-        "36": 10,
-        "41": 16,
-    }.get(ai_prefix)
 
 
 _GS1_APPLICATION_IDENTIFIERS_FILE = (
--- tests/gs1/test_application_identifiers.py
+++ tests/gs1/test_application_identifiers.py
@@ -1,34 +1,7 @@
-from typing import Optional
-
 import pytest
 
 from biip import ParseError
 from biip.gs1 import GS1ApplicationIdentifier
-from biip.gs1._application_identifiers import (  # XXX Not public API yet
-    get_predefined_length,
-)
-
-
-@pytest.mark.parametrize(
-    "value, length",
-    [
-        # Valid data where first element has predefined length
-        ("0195012345678903", 16),
-        ("01950123456789033102000400", 16),
-        ("3102000400", 10),
-        #
-        # Valid data where first element has unknown length
-        ("800500365", None),
-        ("10123456", None),
-        #
-        # Bogus data
-        ("9995012345678903", None),
-        ("9", None),
-        ("", None),
-    ],
-)
-def test_get_predefined_length(value: str, length: Optional[int]) -> None:
-    assert get_predefined_length(value) == length
 
 
 @pytest.mark.parametrize("unknown_ai", ["abcdef", "199999"])
