66e30110963a11b7404553a48f50d4bb4031da59
Tetiana Novikova <nowikowa.tanya@gmail.com>
2024-10-01 10:40:29 -0700

    feature/use-importlib: fix resource file access to support zipped package environments

--- src/biip/gs1/_prefixes.py
+++ src/biip/gs1/_prefixes.py
@@ -12,8 +12,8 @@ from __future__ import annotations
 
 import json
 import lzma
-import pathlib
 from dataclasses import dataclass
+from importlib import resources
 from typing import Dict, Optional, Union
 
 from biip import ParseError
@@ -142,16 +142,17 @@ class _GS1PrefixRange:
     usage: str
 
 
-_GS1_PREFIX_RANGES_FILE = pathlib.Path(__file__).parent / "_prefix_ranges.json"
+def _load_prefix_ranges():
+    with resources.open_text(__package__, '_prefix_ranges.json') as f:
+        data = json.load(f)
+    return [_GS1PrefixRange(**kwargs) for kwargs in data]
 
-_GS1_PREFIX_RANGES = [
-    _GS1PrefixRange(**kwargs)
-    for kwargs in json.loads(_GS1_PREFIX_RANGES_FILE.read_text())
-]
+_GS1_PREFIX_RANGES = _load_prefix_ranges()
 
-_GS1_COMPANY_PREFIX_TRIE_FILE = (
-    pathlib.Path(__file__).parent / "_company_prefix_trie.json.lzma"
-)
+def _load_company_prefix_trie():
+    with resources.open_binary(__package__, '_company_prefix_trie.json.lzma') as compressed_file:
+        decompressed_data = lzma.decompress(compressed_file.read())
+        data = json.loads(decompressed_data.decode('utf-8'))
+    return data
 
-with lzma.open(_GS1_COMPANY_PREFIX_TRIE_FILE) as fh:
-    _GS1_COMPANY_PREFIX_TRIE: _TrieNode = json.load(fh)
+_GS1_COMPANY_PREFIX_TRIE: _TrieNode = _load_company_prefix_trie()
