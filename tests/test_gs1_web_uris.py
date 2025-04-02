78cea67976c7d525cc1163ede8bbbed372d77c25
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-04-02 18:00:57 +0200

    Add GS1WebURI.as_canonical_uri()

--- tests/test_gs1_web_uris.py
+++ tests/test_gs1_web_uris.py
@@ -298,6 +298,31 @@ def test_parse_error(value: str, error: str) -> None:
 
 
 @pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        (
+            "https://example.com/01/614141123452",
+            "https://id.gs1.org/01/00614141123452",
+        ),
+        (
+            "https://example.com/gtin/614141123452",
+            "https://id.gs1.org/01/00614141123452",
+        ),
+        (
+            "https://example.com/gtin/614141123452/cpv/2A/lot/ABC123",
+            "https://id.gs1.org/01/00614141123452/22/2A/10/ABC123",
+        ),
+        (
+            "https://example.com/sscc/106141412345678908?02=00614141123452&37=25&10=ABC123&foo=bar",
+            "https://id.gs1.org/00/106141412345678908?02=00614141123452&37=25&10=ABC123",
+        ),
+    ],
+)
+def test_as_canonical_uri(value: str, expected: str) -> None:
+    assert GS1WebURI.parse(value).as_canonical_uri() == expected
+
+
+@pytest.mark.parametrize(
     ("value", "ai", "expected"),
     [
         ("https://id.gs1.org/gtin/614141123452/lot/ABC123", "01", ["00614141123452"]),
